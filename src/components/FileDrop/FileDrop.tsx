16afd58dd49469d47893281d220ea27991b54521
Grace <gcw.chung@gmail.com>
2023-09-10 03:18:32 -0700

    Add initial file drop functionality

--- src/components/FileDrop/FileDrop.tsx
+++ src/components/FileDrop/FileDrop.tsx
@@ -1,6 +1,63 @@
-import React from "react";
-import styles from "./FileDrop.scss";
+import React, { useState, useEffect, useRef, DragEvent } from "react";
+import styles from "./FileDrop.module.scss";
+import { join } from "path";
 
-export function FileDrop() {
-  return <div className={styles.FileDrop}>FileDrop</div>;
+interface FileDropProps {
+  onUpload: () => void;
+  children?: JSX.Element | JSX.Element[];
+}
+
+export function FileDrop({ onUpload, children }: FileDropProps) {
+  const [dragActive, setDragActive] = useState<boolean>(false);
+  const drop = useRef();
+
+  const handleDrag = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+      e.dataTransfer.setData("Text", e.target.id);
+    } else {
+      setDragActive(false);
+    }
+  };
+
+  const handleDrop = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      const file = e.dataTransfer.files[0];
+    }
+  };
+
+  const readFileAsText = (file: any) => {
+    const reader = new FileReader();
+
+    return new Promise((resolve, reject) => {
+      reader.onerror = () => {
+        reader.abort();
+        reject(new DOMException("Problem parsing input file."));
+      };
+
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+
+      reader.readAsText(file);
+    });
+  };
+
+  return (
+    <div
+      className={styles.FileDrop}
+      ref={drop.current}
+      onDragEnter={handleDrag}
+      onDragLeave={handleDrag}
+      onDragOver={handleDrag}
+      onDrop={handleDrop}
+    ></div>
+  );
 }
