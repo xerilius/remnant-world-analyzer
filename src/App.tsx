dbe97d3850c9925bf6efb504b0680ff190ce5b20
Grace <gcw.chung@gmail.com>
2023-09-11 16:30:53 -0700

    Update logo and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import gameIcon from "./assets/remnant-icon.png";
 import { FileDrop } from "./components";
+import { parseData } from "./utilities/parseData";
+import gameLogo from "./assets/remnant-logo2.png";
+
+import styles from "./App.module.css";
 
 function App() {
+  const [data, setData] = useState(null);
+  const display = data && parseData(data);
+
   return (
-    <div className="App">
-      <header className="App-header">Remnant World Analyzer</header>
-      <FileDrop />
+    <div className={styles.App}>
+      <div className={styles.Header}>
+        <img src={gameLogo} />
+        <div className={styles.Title}>World Analyzer</div>
+      </div>
+      {<FileDrop setData={setData} />}
+      {display}
     </div>
   );
 }
